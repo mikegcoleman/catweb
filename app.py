b4afb5b8b9ff281fd05e612f7767bdea14b87d6d
Gustavo Esteban Borragán <gustavo.borragan@prefapp.es>
2020-11-17 19:26:59 +0000

    Cambio de las imagenes de gatos

--- app.py
+++ app.py
@@ -5,21 +5,15 @@ app = Flask(__name__)
 
 # list of cat images 
 images = [
-    "https://img.buzzfeed.com/buzzfeed-static/static/2014-06/24/17/enhanced/webdr10/anigif_enhanced-29828-1403646582-7.gif",
-    "https://img.buzzfeed.com/buzzfeed-static/static/2014-06/24/17/enhanced/webdr06/anigif_enhanced-28244-1403646370-2.gif",
-    "https://img.buzzfeed.com/buzzfeed-static/static/2014-06/24/17/enhanced/webdr10/anigif_enhanced-29510-1403646649-3.gif",
-    "https://img.buzzfeed.com/buzzfeed-static/static/2014-06/24/17/enhanced/webdr10/anigif_enhanced-30034-1403646599-7.gif",
-    "https://img.buzzfeed.com/buzzfeed-static/static/2014-06/24/17/enhanced/webdr09/anigif_enhanced-4743-1403646817-9.gif",
-    "https://img.buzzfeed.com/buzzfeed-static/static/2014-06/24/17/enhanced/webdr08/anigif_enhanced-23675-1403646767-8.gif",
-    "https://img.buzzfeed.com/buzzfeed-static/static/2014-06/24/17/enhanced/webdr10/anigif_enhanced-29994-1403646740-39.gif",
-    "https://img.buzzfeed.com/buzzfeed-static/static/2014-06/24/17/enhanced/webdr08/anigif_enhanced-23704-1403646787-1.gif",
-    "https://img.buzzfeed.com/buzzfeed-static/static/2014-06/24/18/enhanced/webdr02/anigif_enhanced-10067-1403647307-22.gif",
-    "https://img.buzzfeed.com/buzzfeed-static/static/2014-06/24/18/enhanced/webdr06/anigif_enhanced-28044-1403647272-11.gif",
-    "https://img.buzzfeed.com/buzzfeed-static/static/2014-06/24/18/enhanced/webdr09/anigif_enhanced-4685-1403647551-18.gif",
-    "https://img.buzzfeed.com/buzzfeed-static/static/2014-06/24/18/enhanced/webdr02/anigif_enhanced-24306-1403647326-1.gif",
-    "https://img.buzzfeed.com/buzzfeed-static/static/2014-06/24/18/enhanced/webdr07/anigif_enhanced-3756-1403647724-4.gif",
-    "https://img.buzzfeed.com/buzzfeed-static/static/2014-06/24/17/enhanced/webdr09/anigif_enhanced-4743-1403646233-1.gif",
-    "https://img.buzzfeed.com/buzzfeed-static/static/2014-06/24/17/enhanced/webdr05/anigif_enhanced-2087-1403646837-1.gif"
+        "https://images.pexels.com/photos/1056251/pexels-photo-1056251.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        "https://images.pexels.com/photos/236606/pexels-photo-236606.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        "https://images.pexels.com/photos/1573324/pexels-photo-1573324.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        "https://images.pexels.com/photos/4587959/pexels-photo-4587959.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        "https://images.pexels.com/photos/1543793/pexels-photo-1543793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        "https://images.pexels.com/photos/156321/pexels-photo-156321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        "https://images.pexels.com/photos/1560424/pexels-photo-1560424.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        "https://images.pexels.com/photos/3885948/pexels-photo-3885948.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        "https://images.pexels.com/photos/3069334/pexels-photo-3069334.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
 ]
 
 @app.route('/')
